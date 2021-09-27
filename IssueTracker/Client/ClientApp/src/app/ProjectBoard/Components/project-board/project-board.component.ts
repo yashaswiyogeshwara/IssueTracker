fc3075efee677f21ffcd9e3747bf2f710ee329bf
yogeshwaray <yashaswi.yogeshwara@campsystems.com>
2021-09-27 10:18:05 +0530

    added add icon to project board component.
    used material icons.
    used CSS grid for layout

--- IssueTracker/Client/ClientApp/src/app/ProjectBoard/Components/project-board/project-board.component.ts
+++ IssueTracker/Client/ClientApp/src/app/ProjectBoard/Components/project-board/project-board.component.ts
@@ -1,3 +1,5 @@
+
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,6 +12,24 @@ export class ProjectBoardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi',
+    'Episode IX – The Rise of Skywalker'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    debugger;
+    console.log(event);
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
   }
 
 }
--- IssueTracker/Client/ClientApp/src/app/ProjectBoard/project-board.module.ts
+++ IssueTracker/Client/ClientApp/src/app/ProjectBoard/project-board.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProjectBoardRoutingModule } from './project-board-routing.module';
 import { ProjectBoardComponent } from './Components/project-board/project-board.component';
-
+import {MatIconModule} from '@angular/material/icon';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     ProjectBoardComponent
   ],
   imports: [
-    CommonModule,
-    ProjectBoardRoutingModule
+    CommonModule,  
+    ProjectBoardRoutingModule,
+    DragDropModule,
+    MatIconModule
   ]
 })
 export class ProjectBoardModule { }
