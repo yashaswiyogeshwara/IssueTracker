8297cf68e5a79470f95597896d580d68a744ad65
yogeshwaray <yashaswi.yogeshwara@campsystems.com>
2021-10-13 09:21:34 +0530

    Ticket-28 As a user I should be able to fix display order issue

--- IssueTracker/Api/Controllers/TaskItemController.cs
+++ IssueTracker/Api/Controllers/TaskItemController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Api.Controllers
 {
@@ -21,27 +22,32 @@ namespace Api.Controllers
         [HttpPost]
         public void SaveTask([FromBody] TaskItem taskItem)
         {
-            Context.Add<Core.Models.TaskItem>(new Core.Models.TaskItem()
-            {
-                Title = taskItem.Title,
-                UserId = taskItem.UserId,
-                Description = taskItem.Description,
-                EndDate = taskItem.EndDate,
-                ParentTaskId = taskItem.ParentTaskId,
-                TimeSpent = taskItem.TimeSpent,
-                Estimate = taskItem.Estimate,
-                StartDate = taskItem.StartDate,
-                TeamId = taskItem.TeamId,
-                StatusId = taskItem.StatusId,
-                DisplayOrder = taskItem.DisplayOrder
-            });
-            Context.SaveChanges();    
+                Context.Add<Core.Models.TaskItem>(new Core.Models.TaskItem()
+                {
+                    Title = taskItem.Title,
+                    UserId = taskItem.UserId,
+                    Description = taskItem.Description,
+                    EndDate = taskItem.EndDate,
+                    ParentTaskId = taskItem.ParentTaskId,
+                    TimeSpent = taskItem.TimeSpent,
+                    Estimate = taskItem.Estimate,
+                    StartDate = taskItem.StartDate,
+                    TeamId = taskItem.TeamId,
+                    StatusId = taskItem.StatusId,
+                    DisplayOrder = taskItem.DisplayOrder
+                });
+                Context.SaveChanges();
+                    
         }
 
         [HttpPatch]
-        public void UpdateDisplayOrder([FromBody] UpdateDisplayOrderModel taskItem) {
-            Core.Models.TaskItem task = Context.TaskItems.Where(x => x.Id == taskItem.Id).FirstOrDefault();
-            task.DisplayOrder = taskItem.DisplayOrder;
+        public void UpdateDisplayOrder([FromBody] UpdateDisplayOrderModel[] taskItems) {
+            Dictionary<long, int> taskItemDictionary = taskItems.ToDictionary(x => x.Id, x => x.DisplayOrder);
+            List<Core.Models.TaskItem> tasks = Context.TaskItems.Where(x => taskItemDictionary.Keys.Contains(x.Id)).ToList();
+            foreach (var t in tasks) {
+                t.DisplayOrder = taskItemDictionary[t.Id];
+            }
+            
             Context.SaveChanges();
 
         }
